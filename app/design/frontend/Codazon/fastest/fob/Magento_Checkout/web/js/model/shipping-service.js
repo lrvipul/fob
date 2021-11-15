ed219b766586f819dff6c8820fae0d4a46127de0
lrvipul <vipul@logicrays.com>
2021-11-15 16:33:13 +0530

    default design files of live

---
+++ app/design/frontend/Codazon/fastest/fob/Magento_Checkout/web/js/model/shipping-service.js
@@ -0,0 +1,50 @@
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+define([
+    'jquery',
+    'ko',
+    'Magento_Checkout/js/model/checkout-data-resolver'
+], function ($, ko, checkoutDataResolver) {
+    'use strict';
+
+    var shippingRates = ko.observableArray([]);
+
+    return {
+        isLoading: ko.observable(false),
+
+        /**
+         * Set shipping rates
+         *
+         * @param {*} ratesData
+         */
+        setShippingRates: function (ratesData) {
+            var output = [];
+
+            // for Home delivery restriction
+            if(ratesData.length){                
+                var singlePickupAvailable = false;
+                $.each(ratesData, function(index, item){
+                    if(item.carrier_code != "pickupatstore"){
+                        output.push(item);                        
+                    }
+                });
+            }
+
+            shippingRates(ratesData);
+            shippingRates.valueHasMutated();
+            checkoutDataResolver.resolveShippingRates(ratesData);
+        },
+
+        /**
+         * Get shipping rates
+         *
+         * @returns {*}
+         */
+        getShippingRates: function () {
+            return shippingRates;
+        }
+    };
+});
